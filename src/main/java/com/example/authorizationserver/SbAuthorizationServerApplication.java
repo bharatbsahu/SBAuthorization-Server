e468f12672e7de47f00f1572367ffb04cd677697
bharatbsahu <smartbharat@gmail.com>
2024-06-07 16:37:26 +0530

    oAuth2 Spring Boot Authorization Server

---
+++ src/main/java/com/example/authorizationserver/SbAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.authorizationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbAuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbAuthorizationServerApplication.class, args);
+	}
+
+}
